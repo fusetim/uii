8d16cf40a0bbfbedc7a29a3a2b4b5f561ad1caf8
FuseTim <fusetim.log@gmx.com>
2025-09-09 20:19:30 +0200

    wip: power-cut emergency commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import solidJs from '@astrojs/solid-js';
+import sitemap from '@astrojs/sitemap';
+import Icons from 'unplugin-icons/vite'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [solidJs(), sitemap()],
+  output: 'static',
+  site: 'https://fusetim.github.io',
+  base: `/uii/`,
+  scopedStyleStrategy: 'class',
+  vite: {
+    plugins: [
+      Icons({
+        compiler: 'astro',
+      }),
+    ],
+  }
+});
